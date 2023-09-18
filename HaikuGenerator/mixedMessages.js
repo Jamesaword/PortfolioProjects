73f946f1aa916d23b8c7ed35ae3ca6ab6b31d160
Jamesaword <Jamesaword@users.noreply.github.com>
2023-09-18 15:08:59 -0400

    clean code up

--- HaikuGenerator/mixedMessages.js
+++ HaikuGenerator/mixedMessages.js
@@ -1,11 +1,9 @@
 const storage = require('./lineStorage.js');
 
-//console.log(storage.lineOne[Math.floor(Math.random() * storage.lineOne.length)]);
-//console.log(storage.lineTwo[Math.floor(Math.random() * storage.lineTwo.length)]);
-//console.log(storage.lineThree[Math.floor(Math.random() * storage.lineThree.length)]);
 
 const haiku = () => {
     console.log(
     `${storage.lineOne[Math.floor(Math.random() * storage.lineOne.length)]}\n${storage.lineTwo[Math.floor(Math.random() * storage.lineTwo.length)]}\n${storage.lineThree[Math.floor(Math.random() * storage.lineThree.length)]}`)
 }
+
 haiku();
