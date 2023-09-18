9e8f83e83a40f440d126fc355e316fc9459b2bfc
Jamesaword <Jamesaword@users.noreply.github.com>
2023-09-18 13:33:35 -0400

    First Commit add line database

---
+++ HaikuGenerator/lineStorage.js
@@ -0,0 +1,5 @@
+const lineOne = ['The haiku gardens', 'I have lost my keys', 'Do you believe it?', 'Hippopotamus', 'The sun shines so bright'];
+
+const lineTwo = ["It's snowing out there again", 'Unfamiliarity', 'Individuality', "It's easy if you just try", 'Ocean waves crash on the shore', ];
+
+const lineThree = ['Refrigorator', 'This is the best time', 'Leave it all to me', 'And there you have it', 'Photosynthesis'];
